728a03830ae3ac5142b6b92f2efe6c4e15e744fb
Ines Garcia <ines.cruchinho.garcia@tecnico.ulisboa.pt>
2024-11-24 13:46:43 +0100

    feat: no memory in limit now

--- 05/05_reverse_vowels.c
+++ 05/05_reverse_vowels.c
@@ -5,10 +5,9 @@
 
 char* reverseVowels(char* s) {
     char* s_vowels = malloc(sizeof(char));
-    int* s_positions = malloc(sizeof(int));
     int nr_vowels = 0;
     int len = strlen(s);
-    int i, j;
+    int i, j = 0;
 
     for (i = 0; i < len; i++) {
         char c = tolower(s[i]);
@@ -16,17 +15,17 @@ char* reverseVowels(char* s) {
         if ( c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
             nr_vowels++;
             s_vowels = realloc(s_vowels, sizeof(char) * nr_vowels);
-            s_positions = realloc(s_positions, sizeof(int) * nr_vowels);
-
             s_vowels[nr_vowels - 1] = s[i];
-            s_positions[nr_vowels - 1] = i;
         }
     }
 
-    j = nr_vowels - 1;
-    for (i = 0; i < nr_vowels; i++) {
-        s[s_positions[i]] = s_vowels[j];
-        j--;
+    for (i = len - 1; i >= 0; i--) {
+        char c = tolower(s[i]);
+
+        if ( c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            s[i] = s_vowels[j];
+            j++;
+        }
     }
 
     return s;
